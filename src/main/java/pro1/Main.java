f208e8ca644dd47e0fbdfd9e5cb1031bdc1ec301
Vojta Vorel <vorelvo2@uhk.cz>
2025-03-31 09:30:51 +0200

    Init2

--- src/main/java/pro1/Main.java
+++ src/main/java/pro1/Main.java
@@ -17,21 +17,5 @@ public class Main {
         String room = config.getProperty("room");
         String term = config.getProperty("term");
 
-        for(int i =2014; i <=2022;i++) {
-            HttpClient client = HttpClient.newBuilder().build();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://stag-demo.uhk.cz/ws/services/rest2/rozvrhy/getRozvrhByMistnost?semestr=ZS&rok=" + i + "&budova=J&mistnost=J22&outputFormat=json"))
-                    .build();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            String body = response.body();
-            Gson gson = new Gson();
-            RoomSchedule rozvrhUcebny = gson.fromJson(body, RoomSchedule.class);
-            int sum=0;
-            for(var a : rozvrhUcebny.getActions())
-            {
-                sum+=a.obsazeni;
-            }
-            System.out.println("J22, rok " +i+" : "+sum+" osob");
-        }
     }
 }
