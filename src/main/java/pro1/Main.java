e4f9a99e4eee0a8f7c67c9ea23ca9ae4affde99b
Vojta Vorel <vorelvo2@uhk.cz>
2025-03-31 12:10:27 +0200

    Po cviku 8

--- src/main/java/pro1/Main.java
+++ src/main/java/pro1/Main.java
@@ -4,18 +4,43 @@ import com.google.gson.Gson;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.*;
 import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-
+    public static void main(String[] args)
+            throws IOException, InterruptedException, URISyntaxException {
         Properties config = new Properties();
         config.load(new FileInputStream("app.config"));
         int yearFrom = Integer.parseInt(config.getProperty("yearFrom"));
         int yearTo = Integer.parseInt(config.getProperty("yearTo"));
         String room = config.getProperty("room");
         String term = config.getProperty("term");
+        HttpClient httpClient = HttpClient
+                .newBuilder().build();
+        for(int y = yearFrom; y<=yearTo; y++)
+        {
+            String url="https://stag-demo.uhk.cz/ws/services/rest2/rozvrhy/getRozvrhByMistnost?semestr="+term+"&rok="+y+"&budova=J&mistnost="+room+"&outputFormat=json";
+            HttpRequest httpRequest = HttpRequest
+                    .newBuilder()
+                    .uri(new URI(url)).build();
 
+            HttpResponse<String> response = httpClient.send(
+                    httpRequest,
+                    HttpResponse.BodyHandlers.ofString());
+            //System.out.println(response.body());
+
+            Gson gson = new Gson();
+            Schedule schedule = gson.fromJson(
+                    response.body(),
+                    Schedule.class);
+            int sum = 0;
+            for(ScheduleEvent e : schedule.events)
+            {
+                sum += e.personsCount;
+            }
+            System.out.println("Rok "+y+": "+sum+" osob");
+        }
     }
 }
